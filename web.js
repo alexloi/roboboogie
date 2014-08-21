bd0973e916e675a09299d9e36b5f90a1d7b603ff
alexloi <alex@roboboogie.us>
2014-08-21 12:32:49 +0100

    Added stuff

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
+var path = require('path');
+
+// Express config
+var root = path.normalize(__dirname);
+app.set('views', root);
+app.set('view engine', 'jade');
 
 app.get('/', function(req,res){
-    return res.send('Hola');
+    return res.render('index');
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 3000);
 
 app.listen(port, function(){
-    console.log('Listening on' + port);
+    console.log('Listening on ' + port);
 });
