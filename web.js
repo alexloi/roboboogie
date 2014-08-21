3595bfc2e0bbd090223d99958286c423a4851b7a
alexloi <alex@roboboogie.us>
2014-08-21 12:11:47 +0100

    fixed express

--- web.js
+++ web.js
@@ -2,5 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req,res){
-	return res.send('Hola');
+    return res.send('Hola');
+});
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function(){
+    console.log('Listening on' + port);
 });
